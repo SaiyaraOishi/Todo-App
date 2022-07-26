45f01e65707e58eb1162c14dc487b256b0997725
SaiyaraOishi <saiyara.oishi@gmail.com>
2022-07-26 19:12:25 +0600

    receiving todo properly

--- src/UpdateForm.js
+++ src/UpdateForm.js
@@ -1,13 +1,34 @@
-import React,{useState} from "react";
+import React,{useContext, useEffect, useState} from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-import {Link} from "react-router-dom";
+import {Link,useNavigate,useParams} from "react-router-dom";
+import {UserContext} from "./Context.js";
 
 
 export default function UpdateForm(){
 
     const [title,setTitle]=useState("");
     const [description,setDescription]=useState("");
+    const {id} = useParams();
+    const {todolist}=useContext(UserContext);
+
+    const navigate=useNavigate();
+    const handleUpdateClick = () =>{
+        navigate("/dashboard");
+    }
+
+    const handleCancelClick =( )=>{
+        navigate("/dashboard");
+    }
+
+    // useEffect(()=>{
+    const todo=todolist.find(todo=>todo.id===id);
+    console.log(todo);
+    // console.log(todo.title);
+    // setTitle(todo.title);
+    // console.log(title);
+    // setDescription(todo.description);
+    // },[]);
 
     return (
         <>
@@ -20,18 +41,19 @@ export default function UpdateForm(){
             <form className="form-input">
             <div>
                 <label htmlFor="title">Title</label><br/>
-                <input className="input-todo" type="text" placeholder="Enter title here" name="title" id="title" value={title} />
+                <input className="input-todo" type="text" placeholder="Enter title here" name="title" id="title" />
             </div>
             {/* <div className="error-form">{!titleIsValid && <p>*Title is required</p>}</div> */}
             <br/>
             <br/>
             <div>
                 <label htmlFor="description">Description</label><br/>
-                <input className="input-todo" type="text" placeholder="Enter description here" name="description" id="description" value={description} />
+                <input className="input-todo" type="text" placeholder="Enter description here" name="description" id="description" />
             </div>
             {/* <div className="error-form">{!descriptionIsValid && <p>*Description is required</p>}</div> */}
             <br/>
-            <button type="button" className='button-todo'>Create</button><br />
+            <button type="button" className='button-todo'  onClick={handleCancelClick}>Cancel</button>
+            <button type="button" className='button-todo' onClick={handleUpdateClick}>Update</button><br />
             </form>
         </div>
         <Footer/>
