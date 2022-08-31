063f6315026a91681347ea5250078ab5ed0792ed
SaiyaraOishi <saiyara.oishi@gmail.com>
2022-08-31 17:24:10 +0600

    useFormik hook applied to <CreateTodo/>

--- src/components/CreateTodo.js
+++ src/components/CreateTodo.js
@@ -2,38 +2,45 @@ import Footer from "./Footer";
 import Navbar from "./Navbar";
 import { Link, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import { useState } from "react";
 import { addTodo } from "../redux/todos/actions";
+import { useFormik } from "formik";
 
 export default function CreateTodo() {
 
-    const [title, setTitle] = useState("");
-    const [description, setDescription] = useState("");
     const dispatch = useDispatch();
-
-    const titleIsValid = title.trim().length > 0;
-
     const navigate = useNavigate();
-    const onCreateClick = () => {
-        const current = new Date();
-        const date = `${current.getDate()} ${current.toLocaleString("default", {
-            month: "short",
-        })}, ${current.getFullYear()}`;
-        if (titleIsValid) {
-            dispatch(addTodo({ title: title, description: description, isCompleted: false, createdAt: date }));
+
+    const formik = useFormik({
+        initialValues: {
+            title: "",
+            description: ""
+        },
+        validate: values => {
+            let errors = {};
+            if (!values.title) {
+                errors.title = "*Title is required";
+            }
+            return errors;
+        },
+        onSubmit: values => {
+            const current = new Date();
+            const date = `${current.getDate()} ${current.toLocaleString("default", {
+                month: "short",
+            })}, ${current.getFullYear()}`;
+            dispatch(addTodo({ title: values.title, description: values.description, isCompleted: false, createdAt: date }));
             navigate("/dashboard", { replace: true });
         }
-    }
+    })
 
-    const handleTitleChange = (e) => {
-        setTitle(e.target.value);
-        console.log(e.target.value)
-    }
+    // const handleTitleChange = (e) => {
+    //     setTitle(e.target.value);
+    //     console.log(e.target.value)
+    // }
 
-    const handleDescriptionChange = (e) => {
-        setDescription(e.target.value);
-        console.log(e.target.value)
-    }
+    // const handleDescriptionChange = (e) => {
+    //     setDescription(e.target.value);
+    //     console.log(e.target.value)
+    // }
 
     return (
         <>
@@ -41,19 +48,19 @@ export default function CreateTodo() {
             <div className="relative mt-16 ml-28 mr-28">
                 <Link to="/dashboard" className="text-blue-600 underline">←Back</Link>
                 <div className="mt-4">Create new Todo</div>
-                <form className="mt-12">
+                <form className="mt-12" onSubmit={formik.handleSubmit}>
                     <div>
                         <label htmlFor="title" className="ml-28 ">Title</label><br />
-                        <input type="text" placeholder="Enter title" id="title" className="rounded-sm ml-28 px-96 py-4 border border-black text-left" value={title} onChange={handleTitleChange} />
+                        <input type="text" placeholder="Enter title" id="title" className="rounded-sm ml-28 px-96 py-4 border border-black text-left" value={formik.title} onChange={formik.handleChange} />
                     </div>
-                    <div className="text-red-800 text-center">{!titleIsValid && <p>*Title is required</p>}</div>
+                    {formik.errors.title ? <div>{formik.errors.title}</div> : null}
                     <br />
                     <div>
                         <label htmlFor="description" className="ml-28 ">Description</label><br />
-                        <input type="text" placeholder="Enter description" id="description" className="rounded-sm ml-28 px-96 py-4 border border-black text-left" value={description} onChange={handleDescriptionChange} />
+                        <input type="text" placeholder="Enter description" id="description" className="rounded-sm ml-28 px-96 py-4 border border-black text-left" value={formik.description} onChange={formik.handleChange} />
                     </div>
                     <div className="flex justify-end mt-4 mr-20">
-                        <button type="submit" className="bg-blue-500 px-8 py-1 text-white rounded-md" onClick={onCreateClick}>Create</button>
+                        <button type="submit" className="bg-blue-500 px-8 py-1 text-white rounded-md">Create</button>
                     </div>
                 </form>
             </div>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,5 @@
 import Footer from "./Footer";
 import { useNavigate } from "react-router-dom";
-// import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { addName } from "../redux/user/actions";
 import { useFormik } from "formik";
@@ -33,19 +32,6 @@ export default function Login() {
         }
     });
 
-    // const [name, setName] = useState("");
-
-    // const formIsValid = name.trim().length > 0;
-
-    // const onNextClick = () => {
-    //         dispatch(addName(formik.values.name));
-    //         navigate("/dashboard");
-    // }
-
-    // const handleNameChange = (e) => {
-    //     setName(e.target.value);
-    // }
-
     return (
         <>
             <div className="text-center mt-40 font-bold text-xl">Simple ToDo</div>
@@ -54,7 +40,6 @@ export default function Login() {
                     <input className="rounded-md border border-black text-center px-4 py-2" type="text" placeholder="Enter name" id="name" value={formik.values.name} onChange={formik.handleChange} />
                 </div>
                 {formik.errors.name ? <div className="text-red-800 text-center">{formik.errors.name}</div> : null}
-                {/* <div className="text-red-800 text-center">{!formIsValid && <p>*Name is required</p>}</div> */}
                 <div className="flex justify-center mt-28">
                     <button type="submit" className="bg-blue-500 px-4 py-2 text-white rounded-md">Next</button>
                 </div>
